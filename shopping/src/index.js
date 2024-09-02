bb2714a4d78eabad5efec04d7670dd53f27aec95
chiragparmar2004 <chiragparmar4780@gmail.com>
2024-09-02 12:47:53 +0530

    added shopping service

---
+++ shopping/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { PORT } = require("./config");
+const { databaseConnection } = require("./database");
+const expressApp = require("./express-app");
+const { CreateChannel } = require("./utils");
+
+const StartServer = async () => {
+  const app = express();
+
+  await databaseConnection();
+
+  const channel = await CreateChannel();
+  //   console.log(channel, "created channel");
+  await expressApp(app, channel);
+
+  app
+    .listen(PORT, () => {
+      console.log(`listening to port ${PORT}`);
+    })
+    .on("error", (err) => {
+      console.log(err);
+      process.exit();
+    });
+};
+
+StartServer();
